b9ceff2618b0212137792f67d8e79120b8017f67
Benours <benjamin.baska@gmail.com>
2020-02-10 09:22:46 +0100

    update ben graphique test 1

---
+++ SDL_Test_Graphique/premier_test.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+
+void pause()
+{
+    int continuer = 1;
+    SDL_Event event;
+ 
+    while (continuer)
+    {
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                continuer = 0;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    SDL_Surface *ecran = NULL; // Le pointeur qui va stocker la surface de l'écran
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_WM_SetIcon(SDL_LoadBMP("golf2.bmp"), NULL);
+
+    ecran = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE); // On tente d'ouvrir une fenêtre
+    if (ecran == NULL) // Si l'ouverture a échoué, on le note et on arrête
+    {
+        fprintf(stderr, "Impossible de charger le mode vidéo : %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+    
+    SDL_WM_SetCaption("Golf Mathématique", NULL);
+
+    pause();
+
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
