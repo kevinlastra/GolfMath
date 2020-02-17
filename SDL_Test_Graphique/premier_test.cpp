f12e2e36b55ad2f4b8e137c2e0765cb52b1c1402
Benours <benjamin.baska@gmail.com>
2020-02-17 18:15:25 +0100

    test sdl 2

--- SDL_Test_Graphique/premier_test.cpp
+++ SDL_Test_Graphique/premier_test.cpp
@@ -1,39 +1,92 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <SDL/SDL.h>
-
-void pause()
-{
-    int continuer = 1;
-    SDL_Event event;
- 
-    while (continuer)
-    {
-        SDL_WaitEvent(&event);
-        switch(event.type)
-        {
-            case SDL_QUIT:
-                continuer = 0;
-        }
-    }
-}
+#include <time.h>
 
 int main(int argc, char *argv[])
 {
-    SDL_Surface *ecran = NULL; // Le pointeur qui va stocker la surface de l'écran
+    SDL_Surface *ecran = NULL, *herbe = NULL;
+    SDL_Event event;
 
     SDL_Init(SDL_INIT_VIDEO);
 
-    ecran = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE); // On tente d'ouvrir une fenêtre
-    if (ecran == NULL) // Si l'ouverture a échoué, on le note et on arrête
+    ecran = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_RESIZABLE);
+    if (ecran == NULL)
     {
         fprintf(stderr, "Impossible de charger le mode vidéo : %s\n", SDL_GetError());
         exit(EXIT_FAILURE);
     }
-    
-    SDL_WM_SetCaption("Ma super fenêtre SDL !", NULL);
 
-    pause();
+    SDL_WarpMouse(ecran->w/2, ecran->h/2);
+
+    SDL_WM_SetCaption("Golf Mathématique !", NULL);
+
+    herbe = SDL_LoadBMP("Projet/green/basic/green.bmp");
+
+    SDL_Rect positionHerbe;
+
+    positionHerbe.x = ecran->w / 2 - herbe->w / 2;
+    positionHerbe.y = ecran->h / 2 - herbe->h / 2;
+    
+    SDL_BlitSurface(herbe, NULL, ecran, &positionHerbe);
+
+    int continuer = 1;
+    SDL_EnableKeyRepeat(10, 10);
+
+	while (continuer)
+	{
+		SDL_WaitEvent(&event);
+	    switch(event.type)
+	    {
+	        case SDL_QUIT:
+	            continuer = 0;
+	            break;
+	        case SDL_MOUSEBUTTONDOWN:
+	        	if (event.button.button == SDL_BUTTON_LEFT)
+	        	{
+	        		herbe = SDL_LoadBMP("Projet/green/basic/green_pressed.bmp");
+	        	}
+	        	else if (event.button.button == SDL_BUTTON_RIGHT)
+	        	{
+	        		continuer = 0;
+	        	}
+	        	break;
+	        case SDL_MOUSEMOTION:
+	        	positionHerbe.x = event.motion.x - herbe->w/2;
+	        	positionHerbe.y = event.motion.y - herbe->h/2;
+	        	break;
+	       	case SDL_KEYDOWN:
+	       		switch (event.key.keysym.sym)
+	       		{
+	       			case SDLK_ESCAPE:
+	       				continuer = 0;
+	       				break;
+	       			case SDLK_F11:
+	       				ecran = SDL_SetVideoMode(1280, 720, 32, SDL_FULLSCREEN | SDL_DOUBLEBUF);
+	       				break;
+	       			case SDLK_RIGHT:
+	       				positionHerbe.x += 2;
+	       				break;
+	       			case SDLK_LEFT:
+	       				positionHerbe.x -= 2;
+	       				break;
+	       			case SDLK_DOWN:
+	       				positionHerbe.y += 2;
+	       				break;
+	       			case SDLK_UP:
+	       				positionHerbe.y -= 2;
+	       				break;
+	       		}
+	       		break;
+	    }
+	    SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 0, 0, 0));
+
+	    SDL_BlitSurface(herbe, NULL, ecran, &positionHerbe);
+	    
+	    SDL_Flip(ecran);
+	}
+    
+    SDL_FreeSurface(herbe);
 
     SDL_Quit();
 
